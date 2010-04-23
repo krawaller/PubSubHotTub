5817a3396e65b0561410c21a5d8324bd2caf2eb2
Jacob Waller <litenjacob@gmail.com>
2010-04-23 18:46:09 +0200

    Realtime Rhino testing with Growl notifications

--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -1,8 +1,19 @@
 
 load('/Library/Ruby/Gems/1.8/gems/visionmedia-jspec-2.11.2/lib/jspec.js')
+load('/Library/Ruby/Gems/1.8/gems/visionmedia-jspec-2.11.2/lib/growl.js')
 load('lib/pubsub.hottub.js')
 
 JSpec
 .exec('spec/spec.core.js')
 .run({ formatter: JSpec.formatters.Terminal })
 .report()
+
+Growl.notify(
+	[
+		["Passes", JSpec.stats.passes].join(": "),
+		["Failures", JSpec.stats.failures].join(": ")
+	]
+	.join("\n"),
+
+	{ title: 'pubsub.hottub.js', image: 'Aptana' }
+)
